954f1b2312913f93be8967fb9768d4383a8d5b69
copev313 <copev313@gmail.com>
2022-10-05 18:32:13 -0400

    Updated Docker configuration

--- streaming_titles/main.py
+++ streaming_titles/main.py
@@ -1,18 +1,12 @@
 """
     Main module containing the FastAPI app and lambda handler.
 """
-import os
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum
 
 from database.session import db, engine, Base
 from routes import base, titles
 
-
-stage = os.getenv("STAGE", "dev")
-openapi_prefix = f"/{stage}" if stage else "/"
 
 app = FastAPI(
     title="Streaming Titles API",
@@ -21,22 +15,20 @@ app = FastAPI(
     redoc_url=None,
     description=(
         "A RESTful API for interacting with titles from various "
-        "streaming platforms."),
-    openapi_prefix=openapi_prefix
+        "streaming platforms.")
 )
-
-origins = [
-    "http://localhost:8000",
-    "http://localhost",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:8000",
+        "http://localhost",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.on_event("startup")
 async def startup():
@@ -60,6 +52,3 @@ def root():
 
 app.include_router(base.router)
 app.include_router(titles.router)
-
-# Handler object for lambda integration:
-handler = Mangum(app)
